aa881b72d4e06b5debae49cb995e0b046448516a
Kresimir Lukin <klukyn@gmail.com>
2020-12-02 13:54:20 +0400

    shortened 2 lines

--- day02.py
+++ day02.py
@@ -7,9 +7,7 @@ part1 = part2 = 0
 for line in lines:
     num1, num2, letter, password = re.search('([0-9]+)-([0-9]+) (.): (.+)', line).groups()
     num1, num2 = int(num1), int(num2)
-    if num1 <= password.count(letter) <= num2:
-        part1 += 1
-    if (password[num1-1] == letter) ^ (password[num2-1] == letter):
-        part2 += 1
+    part1 += num1 <= password.count(letter) <= num2
+    part2 += (password[num1-1] == letter) ^ (password[num2-1] == letter)
 
 print(f'Part 1: {part1}, Part 2: {part2}')
