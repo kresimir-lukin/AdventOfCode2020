cc026ee52c0e1020b53a5e92051332ca0b8c898f
Kresimir Lukin <klukyn@gmail.com>
2020-12-04 21:19:39 +0400

    Added solution for day 4

--- day02.py
+++ day02.py
@@ -5,7 +5,7 @@ lines = open(sys.argv[1]).read().splitlines()
 part1 = part2 = 0
 
 for line in lines:
-    num1, num2, letter, password = re.search('([0-9]+)-([0-9]+) (.): (.+)', line).groups()
+    num1, num2, letter, password = re.search('^([0-9]+)-([0-9]+) (.): (.+)$', line).groups()
     num1, num2 = int(num1), int(num2)
     part1 += num1 <= password.count(letter) <= num2
     part2 += (password[num1-1] == letter) ^ (password[num2-1] == letter)
---
+++ day04.py
@@ -0,0 +1,23 @@
+import sys, re
+
+mandatory_field_validators = {
+    'byr': lambda x: re.match('^\d{4}$', x) and 1920 <= int(x) <= 2002,
+    'iyr': lambda x: re.match('^\d{4}$', x) and 2010 <= int(x) <= 2020,
+    'eyr': lambda x: re.match('^\d{4}$', x) and 2020 <= int(x) <= 2030,
+    'hgt': lambda x: (re.match('^\d{3}cm$', x) and 150 <= int(x.replace('cm', '')) <= 193) or (re.match('^\d{2}in$', x) and 59 <= int(x.replace('in', '')) <= 76),
+    'hcl': lambda x: re.match('^#[0-9a-f]{6}$', x),
+    'ecl': lambda x: x in ('amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'),
+    'pid': lambda x: re.match('^\d{9}$', x)
+}
+
+assert len(sys.argv) == 2
+passport_lines = open(sys.argv[1]).read().replace('\r\n', '\n').replace('\n\n', '|').replace('\n', ' ').split('|')
+passports = []
+for line in passport_lines:
+    passport = {field.split(':')[0]: field.split(':')[1] for field in line.split()}
+    passports.append(passport)
+
+part1 = sum(all(key in passport for key in mandatory_field_validators) for passport in passports)
+part2 = sum(all(func(passport.get(key, '')) for key, func in mandatory_field_validators.items()) for passport in passports)
+
+print(f'Part 1: {part1}, Part 2: {part2}')
