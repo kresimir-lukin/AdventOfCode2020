6b97fd61fe5677155b3a0b6c6a5d9c7456ae88f0
Kresimir Lukin <klukyn@gmail.com>
2020-12-04 12:34:29 +0400

    simplified solution for day 3

--- day03.py
+++ day03.py
@@ -1,19 +1,17 @@
 import sys, functools, operator
 
-def count_trees(forest, slope_r, slope_c):
+def count_trees(forest, slopes):
     rows, cols = len(forest), len(forest[0])
-    row = col = 0
-    trees = 0
-    while row < rows:
-        trees += forest[row][col] == '#'
-        row += slope_r
-        col = (col + slope_c) % cols
-    return trees
+    slope_trees = []
+    for sr, sc in slopes:
+        trees = sum(forest[i * sr][(i * sc) % cols] == '#' for i in range(rows // sr))
+        slope_trees.append(trees)
+    return functools.reduce(operator.mul, slope_trees, 1)
 
 assert len(sys.argv) == 2
 forest = open(sys.argv[1]).read().splitlines()
 
-part1 = count_trees(forest, 1, 3)
-part2 = [count_trees(forest, sr, sc) for sr, sc in [(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)]]
+part1 = count_trees(forest, [(1, 3)])
+part2 = count_trees(forest, [(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)])
 
-print(f'Part 1: {part1}, Part 2: {functools.reduce(operator.mul, part2, 1)}')
+print(f'Part 1: {part1}, Part 2: {part2}')
