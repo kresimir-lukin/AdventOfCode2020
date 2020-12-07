4850ff922c3df0d4de86272fed68e93f63da9a2e
Kresimir Lukin <klukyn@gmail.com>
2020-12-07 13:05:50 +0400

    added solution for day 7

---
+++ day07.py
@@ -0,0 +1,26 @@
+import sys, re, collections
+
+def part1(rules):
+    queue, reachable = collections.deque(['shiny gold']), set(['shiny gold'])
+    while queue:
+        color = queue.pop()
+        for c in rules.get(color, []):
+            if c not in reachable:
+                reachable.add(c)
+                queue.appendleft(c)
+    return len(reachable) - 1
+
+def part2(rules, color = 'shiny gold'):
+    return sum(number + number * part2(rules, c) for c, number in rules[color].items())
+
+assert len(sys.argv) == 2
+input_rules = open(sys.argv[1]).read().splitlines()
+
+rules, inverse_rules = {}, {}
+for input_rule in input_rules:
+    color, rule_colors = input_rule.split(' bags contain ')
+    rules[color] = {color: int(number) for number, color in re.findall('(\d+) (\w+ \w+)', rule_colors)}
+    for c in rules[color]:
+        inverse_rules[c] = inverse_rules.get(c, []) + [color]
+
+print(f'Part 1: {part1(inverse_rules)}, Part 2: {part2(rules)}')
