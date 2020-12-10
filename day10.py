5b9c2384fe74e5b1a11d5d5ef3187ebe803b0756
Kresimir Lukin <klukyn@gmail.com>
2020-12-10 12:32:16 +0400

    Simplified part 2 for day 10

--- day10.py
+++ day10.py
@@ -10,9 +10,7 @@ def part1(joltages):
 def part2(joltages):
     ways = [1] + [0] * (len(joltages) - 1)
     for i in range(1, len(joltages)):
-        for j in range(1, 4):
-            if i - j >= 0 and joltages[i] - joltages[i - j] <= 3:
-                ways[i] += ways[i - j]
+        ways[i] += sum(ways[i-j] for j in range(1, 4) if i-j >= 0 and joltages[i] - joltages[i-j] <= 3)
     return ways[-1]
 
 assert len(sys.argv) == 2
